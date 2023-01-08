00f5c90d105c199790c64311ebbc25224fe06233
jcaceres <jonca5995@gmail.com>
2023-01-08 23:31:27 +0100

    Quitado el pie de página y añadida la hora a la cabecera

--- app/Views/modales/deseoCard.php
+++ app/Views/modales/deseoCard.php
@@ -1,7 +1,9 @@
 <div class="card col-sm-12 col-md-6">
-	<div class="card-header"><?= $deseo->jugador->nombre ?></div>
+	<div class="card-header d-flex justify-content-between">
+		<?= $deseo->jugador->nombre ?>
+		<span class="text-muted"><?= $deseo->updated_at->humanize() ?></span>
+	</div>
 	<card-body>
 		<p class="card-text"><?= $deseo->descripcion ?></p>
 	</card-body>
-	<div class="card-footer text-muted"><?= $deseo->updated_at->humanize() ?></div>
 </div>
